11e53bd0a95ba086337da86c8e38be82b03c5f9e
Toast <carballidoj92@gmail.com>
2026-02-20 11:58:47 -0600

    Added Zod validation to user input

--- server/src/controllers/chat.controllers.ts
+++ server/src/controllers/chat.controllers.ts
@@ -1,17 +1,27 @@
-import { fastify, type FastifyReply, type FastifyRequest } from "fastify";
+import { type FastifyReply, type FastifyRequest } from "fastify";
 import { llm } from "../services/llm.services.js";
+import z from "zod";
 
 type RequestBody = {
   message: string
 }
 
+const MessageSchema = z.object({
+  message: z
+    .string()
+    .trim()
+    .min(1,"Message cannot be emtpy")
+    .max(200,"Maximum message length exceeded")
+}).strict()
+
 export async function createChat (
   request: FastifyRequest<{ Body: RequestBody }>,
   reply: FastifyReply
 ) {
-  const { message } = request.body
-  console.log("Message in the body: ", message)
   try {
+    const body = MessageSchema.parse(request.body)
+    const { message } = body
+    console.log("Message in the body: ", message)
     const response = await llm(message)    
     return { response }
   } catch (error) {

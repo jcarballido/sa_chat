fe035f368ce7d01739947bf3599b9609a74e36d4
GzusNice <66447077+jcarballido@users.noreply.github.com>
2026-04-21 19:47:09 +0000

    Added logout slider

--- ui/src/features/chat/BottomFixedUserComponent.tsx
+++ ui/src/features/chat/BottomFixedUserComponent.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, type MouseEventHandler } from 'react';
+import LogoutSVG from '../../assets/logout.svg'
+import { supabase } from '../../supabase/client';
 
 interface BottomFixedUserComponentProps {
   children?: React.ReactNode;
@@ -6,10 +8,25 @@ interface BottomFixedUserComponentProps {
 }
 
 const BottomFixedUserComponent: React.FC<BottomFixedUserComponentProps> = ({ children, style }) => {
+  const [ showSlide, setShowSlide ] = useState(false)
+
+  const handleSignOut: MouseEventHandler = async (e) => {
+    e.preventDefault()
+    const {error} = await supabase.auth.signOut()
+    if(error){
+      console.log("ERROR DURING SIGN OUT")
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="shadow-lg flex justify-start  gap-2" style={style}>
+    <div className="shadow-lg flex justify-start gap-2 relative" style={style}>
+      <button className={`w-full p-2 bg-gray-400 rounded-xl absolute flex gap-2 -translate-y-[120%] origin-bottom transition duration-150 ease-in ${showSlide ? "scale-y-100":"scale-y-0"} `} onClick={handleSignOut}>
+        <img src={LogoutSVG} className={`transition delay-150 ease-in ${showSlide ? "text-white":"text-transparent"}`}/>
+        <div className={`transition delay-150 ease-in ${showSlide ? "text-white":"text-transparent"} font-semibold`}>Sign Out</div>
+      </button>
       <div className='aspect-square h-8 rounded-full bg-amber-400  ' />
-      <div className='whitespace-nowrap flex items-center justify-start'>
+      <div className={`whitespace-nowrap flex items-center justify-start `} onClick={()=>setShowSlide(!showSlide)}>
         F. LName
       </div>
     </div>
